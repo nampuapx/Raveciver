3a5fcd9957425921a8853c94a2bf253c2f5b4ac6
kolio <apxuenuckon@gmail.com>
2017-05-12 02:00:53 +0300

    midi_lib_config.h moove and proj fix

---
+++ Src/midi_lib_config.h
@@ -0,0 +1,65 @@
+
+
+
+/*******************************************************************************
+ *
+ * MIDI_lib: is midi lib
+ *
+ *    MIDI_lib:                      copyright 2017       Vetrov Nikolay.
+ *
+ *    Permission to use, copy, modify, and distribute this software in source
+ *    and binary forms and its documentation for any purpose and without fee
+ *    is hereby granted, provided that the above copyright notice appear
+ *    in all copies and that both that copyright notice and this permission
+ *    notice appear in supporting documentation.
+ *
+ *    THIS SOFTWARE IS PROVIDED BY THE VETROV NIKOLAY AND CONTRIBUTORS "AS IS"
+ *    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *    PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL VETROV NIKOLAY OR CONTRIBUTORS BE
+ *    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ *    THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************/
+
+#ifndef MIDI_LIB_CONFIG_H
+#define	MIDI_LIB_CONFIG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+    
+
+#define MIDI_OUTGOING_ENABLE
+#define MIDI_INPUT_ENABLE
+
+#define PUT_MIDI_STATUS_BYTE_OPTIMIZATION_ENABLE
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MIDI_LIB_CONFIG_H */
+
+
+// midi_lib_config.h example 
+//////////////////////////////////////////////////////
+                                                        //
+//#define MIDI_OUT_EMPTY_EXAMPLE  // enable this macro for compilation test
+
+
+
+
+
+
+
+
+
